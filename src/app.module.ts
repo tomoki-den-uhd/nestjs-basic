986a0f31510b4eb1e32abf3d6f3b197c69167a77
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-07 16:38:41 +0900

    商品コントローラーの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ItemsController } from './items/items.controller';
 import { ItemsModule } from './items/items.module';
-
 
 @Module({
   imports: [ItemsModule],
