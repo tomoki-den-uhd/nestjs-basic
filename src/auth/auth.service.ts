eca0409e38feff9d7a5fe9c687bed47027414f91
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-08 15:33:44 +0900

    44. パスワードのハッシュ化

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'generated/prisma';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,11 +10,12 @@ export class AuthService {
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
     const { name, email, password, status } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
     return await this.prismaService.user.create({
       data: {
         name,
         email,
-        password,
+        password: hashedPassword,
         status,
       },
     });
