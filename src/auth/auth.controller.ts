b8974319c2ee4b0f3029e0a2d894faed4a887b6b
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-08 15:27:26 +0900

    43. ユーザー作成機能の実装 Part2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from 'generated/prisma';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.authService.createUser(createUserDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from 'generated/prisma';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { name, email, password, status } = createUserDto;
+    return await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password,
+        status,
+      },
+    });
+  }
+}
