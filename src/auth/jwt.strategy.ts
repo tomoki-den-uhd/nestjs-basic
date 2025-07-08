070d094b627017b93df13e765fea7f20b8eebe65
tomoki-den-uhd <tomoki.den.uhd@gmail.com>
2025-07-08 16:52:31 +0900

    51. JWT認証の実装 Part1

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayLoad } from './types/jwtPayload';
+import { RequestUser } from './types/requestUser';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    //親クラスを呼び出す
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET!,
+    });
+  }
+
+  async validate(payload: JwtPayLoad): Promise<RequestUser> {
+    return { id: payload.sub, name: payload.sub, status: payload.status };
+  }
+}
